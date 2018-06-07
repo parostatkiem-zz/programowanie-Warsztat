787286eb584e5e9fbb729c55b4f576f15d1bafdd
parostatkiem <jansudczak@gmail.com>
2018-06-07 10:57:01 +0200

    Initial MVP commit

---
+++ WorkshopManager/Presenter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WorkshopManager
+{
+    class Presenter
+    {
+        Form view;
+        Model model;
+        public Presenter(Model model, Form view)
+        {
+        }
+    }
+}
---
+++ WorkshopManager/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WorkshopManager
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Model model = new Model();
+            MainView view = new MainView();
+            Presenter p = new Presenter(model, view);
+            Application.Run(view);
+            Application.Run(new MainView());
+        }
+    }
+}
